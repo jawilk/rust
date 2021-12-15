5eb70ae1194cad4754fee38d6740e7b33244ab37
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-12-14 18:42:41 -0800

    [SOL] CI: build and test sbf target

--- library/core/tests/lib.rs
+++ library/core/tests/lib.rs
@@ -90,14 +90,14 @@
 #![feature(const_slice_from_ref)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
-#[cfg(not(target_arch = "bpf"))]
+#[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))]
 extern crate test;
 
 mod alloc;
 mod any;
 mod array;
 mod ascii;
-#[cfg(not(target_arch = "bpf"))]
+#[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))]
 mod atomic;
 mod bool;
 mod cell;
